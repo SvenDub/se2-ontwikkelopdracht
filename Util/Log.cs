1d98d6e10bf67d47f5b4b1c016666746f5a2a4eb
Sven Dubbeld <sven@bitsensor.io>
2016-05-23 17:33:06 +0200

    Improved logging output

--- Util/Log.cs
+++ Util/Log.cs
@@ -4,15 +4,15 @@ namespace Ontwikkelopdracht.Models
 {
     public static class Log
     {
-        public static void I(string tag, string msg) => PrintLn(tag, msg, ConsoleColor.DarkGreen);
-        public static void D(string tag, string msg) => PrintLn(tag, msg, ConsoleColor.Blue);
-        public static void W(string tag, string msg) => PrintLn(tag, msg, ConsoleColor.Yellow);
-        public static void E(string tag, string msg) => PrintLn(tag, msg, ConsoleColor.Red);
+        public static void I(string tag, string msg) => PrintLn(tag, "INFO ", msg, ConsoleColor.DarkGreen);
+        public static void D(string tag, string msg) => PrintLn(tag, "DEBUG", msg, ConsoleColor.Blue);
+        public static void W(string tag, string msg) => PrintLn(tag, "WARN ", msg, ConsoleColor.Yellow);
+        public static void E(string tag, string msg) => PrintLn(tag, "ERROR", msg, ConsoleColor.Red);
 
-        public static void PrintLn(string tag, string msg, ConsoleColor color)
+        public static void PrintLn(string tag, string level, string msg, ConsoleColor color)
         {
             Console.ForegroundColor = color;
-            Console.Write($"[{tag.PadRight(10).Substring(0, 10)}] ");
+            Console.Write($"{DateTime.Now.ToString("HH:mm:ss:fff")} [{tag.PadRight(10).Substring(0, 10)}] [{level}] ");
             Console.ResetColor();
             Console.WriteLine(msg);
         }
