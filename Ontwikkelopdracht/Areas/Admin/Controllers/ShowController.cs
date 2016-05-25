879c75b5d11c0c17d27798a47d5f0cd348c36e30
Sven Dubbeld <sven@bitsensor.io>
2016-05-25 18:30:27 +0200

    Add new shows

--- Ontwikkelopdracht/Areas/Admin/Controllers/ShowController.cs
+++ Ontwikkelopdracht/Areas/Admin/Controllers/ShowController.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Web.Mvc;
+using Inject;
 using Ontwikkelopdracht.Models;
+using Ontwikkelopdracht.Persistence;
 
 namespace Ontwikkelopdracht.Areas.Admin.Controllers
 {
     public class ShowController : EntityController<Show>
     {
+        private readonly IRepository<Film> _filmRepository = Injector.Resolve<IRepository<Film>>();
+
         public ActionResult Index() => View(Repository.FindAll());
 
         public ActionResult Search(string query)
@@ -17,5 +21,28 @@ namespace Ontwikkelopdracht.Areas.Admin.Controllers
                             show.Film.Title.ToLower().IndexOf(query, StringComparison.InvariantCultureIgnoreCase) >= 0));
 
         public ActionResult Details(int id) => View(Repository.FindOne(id));
+
+        public ActionResult Add()
+        {
+            ViewBag.Film = new SelectList(_filmRepository.FindAll(), "Id", "Title");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Save(Show show)
+        {
+            show.Film = _filmRepository.FindOne(show.Film.Id);
+            Show saved = Repository.Save(show);
+
+            return RedirectToAction("Details", new {id = saved.Id});
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            Repository.Delete(id);
+
+            return RedirectToAction("Index");
+        }
     }
 }
