42f5e3134ba307b9e8222b9f186b27eea9996f16
Sven Dubbeld <sven@bitsensor.io>
2016-05-25 17:29:45 +0200

    Show list of shows

---
+++ Ontwikkelopdracht/Areas/Admin/Controllers/ShowController.cs
@@ -0,0 +1,10 @@
+﻿using System.Web.Mvc;
+using Ontwikkelopdracht.Models;
+
+namespace Ontwikkelopdracht.Areas.Admin.Controllers
+{
+    public class ShowController : EntityController<Show>
+    {
+        public ActionResult Index() => View(Repository.FindAll());
+    }
+}
