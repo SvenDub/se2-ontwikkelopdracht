7725c4de83c1a0b639644f5be67974bfc10c4e02
Sven Dubbeld <sven@bitsensor.io>
2016-05-25 17:31:39 +0200

    Search shows

--- Ontwikkelopdracht/Areas/Admin/Controllers/ShowController.cs
+++ Ontwikkelopdracht/Areas/Admin/Controllers/ShowController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 using Ontwikkelopdracht.Models;
 
 namespace Ontwikkelopdracht.Areas.Admin.Controllers
@@ -6,5 +7,13 @@ namespace Ontwikkelopdracht.Areas.Admin.Controllers
     public class ShowController : EntityController<Show>
     {
         public ActionResult Index() => View(Repository.FindAll());
+
+        public ActionResult Search(string query)
+            =>
+            View("Index",
+                Repository
+                    .FindAllWhere(
+                        show =>
+                            show.Film.Title.ToLower().IndexOf(query, StringComparison.InvariantCultureIgnoreCase) >= 0));
     }
 }
