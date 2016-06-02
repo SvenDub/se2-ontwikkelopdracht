1040824d46e60a92643d632219b564dc9206cfd7
Sven Dubbeld <sven@bitsensor.io>
2016-06-02 12:08:03 +0200

    Move ConnectionParams to Persistence.MySql

--- Ontwikkelopdracht/Global.asax.cs
+++ Ontwikkelopdracht/Global.asax.cs
@@ -4,7 +4,6 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 using Inject;
-using Ontwikkelopdracht.Models;
 using Ontwikkelopdracht.Persistence;
 using Ontwikkelopdracht.Persistence.MySql;
 using Util;
