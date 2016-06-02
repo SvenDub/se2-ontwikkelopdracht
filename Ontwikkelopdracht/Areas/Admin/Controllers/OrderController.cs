f2e6d9b3573c40adf68380ab221e3a1d7136872f
Sven Dubbeld <sven@bitsensor.io>
2016-06-02 13:44:56 +0200

    Show orders

---
+++ Ontwikkelopdracht/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+using Inject;
+using Ontwikkelopdracht.Models;
+using Ontwikkelopdracht.Persistence;
+using Util;
+
+namespace Ontwikkelopdracht.Areas.Admin.Controllers
+{
+    public class OrderController : EntityController<Order>
+    {
+        private readonly IRepository<Ticket> _ticketRepository = Injector.Resolve<IRepository<Ticket>>();
+
+        public ActionResult Index() => View(Repository.FindAll());
+
+        public ActionResult Search(string query)
+            => View("Index", Repository.FindAllWhere(order => order.User.Name.ContainsIgnoreCase(query)));
+
+        public ActionResult Details(int id)
+        {
+            Order order = Repository.FindOne(id);
+            order.Tickets = _ticketRepository.FindAllWhere(ticket => ticket.Order == id);
+
+            return View(order);
+        }
+    }
+}
