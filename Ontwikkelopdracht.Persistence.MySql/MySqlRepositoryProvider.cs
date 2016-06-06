db66bc9a7d5af6734fe5a316b70806181102041c
Sven Dubbeld <sven@bitsensor.io>
2016-06-06 10:50:45 +0200

    Move entity detection to Persistence

---
+++ Ontwikkelopdracht.Persistence.MySql/MySqlRepositoryProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Ontwikkelopdracht.Persistence.MySql
+{
+    public class MySqlRepositoryProvider : IRepositoryProvider
+    {
+        public Type GetDatabaseType<T>() where T : new() => typeof(MySqlRepository<T>);
+        public Type ConnectionParamsContract => typeof(IMySqlConnectionParams);
+        public Type ConnectionParamsImpl => typeof(ProductionMySqlConnectionParams);
+    }
+}
