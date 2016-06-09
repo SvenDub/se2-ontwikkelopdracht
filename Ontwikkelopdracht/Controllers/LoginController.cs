8a4252c349c370d997f11e4997b791e78d1335cd
Sven Dubbeld <sven@bitsensor.io>
2016-06-09 14:45:02 +0200

    Add signup page

--- Ontwikkelopdracht/Controllers/LoginController.cs
+++ Ontwikkelopdracht/Controllers/LoginController.cs
@@ -2,6 +2,8 @@
 using System.Linq;
 using System.Web.Mvc;
 using Ontwikkelopdracht.Models;
+using Ontwikkelopdracht.Persistence.Exception;
+using Util;
 
 namespace Ontwikkelopdracht.Controllers
 {
@@ -46,7 +48,7 @@ namespace Ontwikkelopdracht.Controllers
                 }
                 else
                 {
-                    return RedirectToAction("Index", new { returnUrl = returnUrl, failed = true });
+                    return RedirectToAction("Index", new {returnUrl = returnUrl, failed = true});
                 }
             }
             else
@@ -60,6 +62,73 @@ namespace Ontwikkelopdracht.Controllers
             Session.Remove(SessionVars.User);
 
             return RedirectToAction("Index");
+        }
+
+        public ActionResult Signup(string returnUrl, bool failed = false)
+        {
+            if (Session[SessionVars.User] == null)
+            {
+                ViewBag.RedirectUrl = returnUrl;
+                ViewBag.Failed = failed;
+                return View();
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(returnUrl))
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                return Redirect(returnUrl);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Signup(User model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                // Make sure the user can not create an admin account or override an existing one.
+                model.Admin = false;
+                model.Id = 0;
+
+                try
+                {
+                    Repository.Save(model);
+                }
+                catch (DataSourceException ex)
+                {
+                    Log.E("SIGNUP", ex.ToString());
+                    return RedirectToAction("Signup", new {returnUrl = returnUrl, failed = true});
+                }
+
+                List<User> users =
+                    Repository.FindAllWhere(user => user.Email == model.Email && user.Password == model.Password);
+                if (users.Count == 1)
+                {
+                    User loggedIn = users.First();
+
+                    Session[SessionVars.User] = loggedIn;
+
+                    if (string.IsNullOrEmpty(returnUrl))
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    return Redirect(returnUrl);
+                }
+                else
+                {
+                    return RedirectToAction("Index", new {returnUrl = returnUrl, failed = true});
+                }
+            }
+            else
+            {
+                // Make sure the user can not create an admin account or override an existing one.
+                model.Admin = false;
+                model.Id = 0;
+                ViewBag.Failed = true;
+
+                return View(model);
+            }
         }
 
         public ActionResult Unauthorized(string returnUrl)
--- Ontwikkelopdracht/Models/User.cs
+++ Ontwikkelopdracht/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Web.Security;
 using Ontwikkelopdracht.Persistence;
 using DataType = System.ComponentModel.DataAnnotations.DataType;
 
@@ -13,14 +14,21 @@ namespace Ontwikkelopdracht.Models
 
         [DataMember(Column = "EMAIL")]
         [DisplayName("Email")]
-        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Emailadres is ongeldig")]
+        [Required]
         public string Email { get; set; }
 
         [DataMember(Column = "NAAM")]
+        [Required]
         [DisplayName("Naam")]
         public string Name { get; set; }
 
         [DataMember(Column = "WACHTWOORD")]
+        [Required]
+        [MembershipPassword(MinRequiredPasswordLength = 8,
+             MinPasswordLengthError =
+                 "Wachtwoord moet minimaal 8 tekens zijn en een hoofdletter en kleine letter bevatten.",
+             MinRequiredNonAlphanumericCharacters = 0)]
         [DisplayName("Wachtwoord")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
