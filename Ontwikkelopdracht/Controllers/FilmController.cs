d8b66109d25ba0fd53d542f28500accb502ae3f9
Sven Dubbeld <sven@bitsensor.io>
2016-06-09 11:37:08 +0200

    Search a genre

--- Ontwikkelopdracht/Controllers/FilmController.cs
+++ Ontwikkelopdracht/Controllers/FilmController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
 using Inject;
 using Ontwikkelopdracht.Models;
@@ -10,11 +10,27 @@ namespace Ontwikkelopdracht.Controllers
     public class FilmController : EntityController<Film>
     {
         private readonly IRepository<Show> _showRepository = Injector.Resolve<IRepository<Show>>();
+        private readonly IRepository<Genre> _genreRepository = Injector.Resolve<IRepository<Genre>>();
 
-        public ActionResult Index() => View(Repository.FindAll());
+        public ActionResult Index()
+        {
+            ViewBag.Genres = _genreRepository.FindAll();
 
-        public ActionResult Search(string query)
-            => View("Index", Repository.FindAllWhere(film => film.Title.ContainsIgnoreCase(query)));
+            return View(Repository.FindAll());
+        }
+
+        public ActionResult Search(string query, int genre = -1)
+        {
+            ViewBag.Genres = _genreRepository.FindAll();
+            ViewBag.Genre = genre;
+
+            List<Film> films = Repository.FindAllWhere(film => film.Title.ContainsIgnoreCase(query));
+            if (genre > -1)
+            {
+                films.RemoveAll(film => film.Genre.Id != genre);
+            }
+            return View("Index", films);
+        }
 
         public ActionResult Details(int id)
         {
