b2010014804734e9485430e6e09e5a7bf704b548
Sven Dubbeld <sven@bitsensor.io>
2016-06-10 13:40:18 +0200

    Edit show

--- Ontwikkelopdracht/Areas/Admin/Controllers/ShowController.cs
+++ Ontwikkelopdracht/Areas/Admin/Controllers/ShowController.cs
@@ -25,6 +25,13 @@ namespace Ontwikkelopdracht.Areas.Admin.Controllers
             return View();
         }
 
+        public ActionResult Edit(int id)
+        {
+            Show show = Repository.FindOne(id);
+            ViewBag.Film = new SelectList(_filmRepository.FindAll(), "Id", "Title", show.Film.Id);
+            return View(show);
+        }
+
         [HttpPost]
         public ActionResult Save(Show show)
         {
