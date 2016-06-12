03c2379fe51aaa7b9c2edbbf3cb077f5401f8451
Sven Dubbeld <sven@bitsensor.io>
2016-06-12 19:01:25 +0200

    Document Ontwikkelopdracht.Persistence

--- Ontwikkelopdracht.Persistence/IdentityAttribute.cs
+++ Ontwikkelopdracht.Persistence/IdentityAttribute.cs
@@ -2,9 +2,15 @@
 
 namespace Ontwikkelopdracht.Persistence
 {
+    /// <summary>
+    ///     A field that is the primary key of an entity.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Property)]
     public class IdentityAttribute : Attribute
     {
+        /// <summary>
+        ///     The name of the column that contains the data.
+        /// </summary>
         public string Column { get; set; }
     }
 }
