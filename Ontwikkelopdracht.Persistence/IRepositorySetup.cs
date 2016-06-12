f4878fa336cdcf1b08d0c83c4f25a31f5ccc8ccd
Sven Dubbeld <sven@bitsensor.io>
2016-06-12 13:23:44 +0200

    Add comments to IRepositorySetup

--- Ontwikkelopdracht.Persistence/IRepositorySetup.cs
+++ Ontwikkelopdracht.Persistence/IRepositorySetup.cs
@@ -1,7 +1,14 @@
 ﻿namespace Ontwikkelopdracht.Persistence
 {
+    /// <summary>
+    ///     Provides the setup for a persistence layer.
+    /// </summary>
     public interface IRepositorySetup
     {
+        /// <summary>
+        ///     Perform the setup for the persistence layer.
+        /// </summary>
+        /// <returns>true on success, false otherwise</returns>
         bool Setup();
     }
 }
