8d340a1b4fc51bcaa4332e56a02ecb1b3806e34d
Sven Dubbeld <sven@bitsensor.io>
2016-06-12 18:04:24 +0200

    Document Inject

--- Inject/TypeNotResolvedException.cs
+++ Inject/TypeNotResolvedException.cs
@@ -2,24 +2,38 @@
 
 namespace Inject
 {
+    /// <summary>
+    ///     Thrown when a Type could not be resolved by the <see cref="Injector"/>.
+    /// </summary>
     public class TypeNotResolvedException : Exception
     {
+        /// <summary>
+        ///     The Type that could not be resolved.
+        /// </summary>
         public Type Type { get; }
 
+        /// <param name="t">The Type that could not be resolved.</param>
         public TypeNotResolvedException(Type t) : this(t, $"Could not resolve \"{t}\".")
         {
         }
 
+        /// <param name="t">The Type that could not be resolved.</param>
+        /// <param name="message">Additional message.</param>
         public TypeNotResolvedException(Type t, string message) : base(message)
         {
             Type = t;
         }
 
+        /// <param name="t">The Type that could not be resolved.</param>
+        /// <param name="innerException">The exception that caused this exception.</param>
         public TypeNotResolvedException(Type t, Exception innerException)
             : this(t, $"Could not resolve \"{t}\".", innerException)
         {
         }
 
+        /// <param name="t">The Type that could not be resolved.</param>
+        /// <param name="message">Additional message.</param>
+        /// <param name="innerException">The exception that caused this exception.</param>
         public TypeNotResolvedException(Type t, string message, Exception innerException)
             : base(message, innerException)
         {
